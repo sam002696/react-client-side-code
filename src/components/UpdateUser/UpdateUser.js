1012e2914ba563b9d61e322ec5c33f619fd86dc6
sam002696 <sadmansakib221@gmail.com>
2021-10-23 17:09:34 +0600

    first commit

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
+import { useEffect } from 'react/cjs/react.development';
 
 const UpdateUser = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/users/${id}`)
+            .then(res => res.json())
+            .then(data => setUser(data));
+    }, [])
     return (
         <div>
             <h2>This is Update User</h2>
+            <h1>Update Name : {user.name}</h1>
         </div>
     );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
+    const [users, setUser] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, [])
+    const handleDeleteItem = (id) => {
+        const proceed = window.confirm("You sure you want to delete?")
+        if (proceed) {
+            const url = `http://localhost:5000/users/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingUsers = users.filter(user => user._id !== id)
+                        setUser(remainingUsers);
+                    }
+                })
+        }
+    }
     return (
         <div>
             <h2>This is Users</h2>
+            <h2>Total Users : {users.length}</h2>
+            {
+                users.map(user => <li key={user._id}>{user.name} :: {user.email} <button onClick={() => handleDeleteItem(user._id)}>Delete</button> <Link to={`/users/update/${user._id}`}><button>Details</button></Link> </li>)
+            }
         </div>
     );
 };
